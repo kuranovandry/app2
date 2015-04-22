006082852716bc7088c6e6f8c2031725ffc42b2c
Andrej Kuranov <a.kuranov@svitla.com>
2015-04-22 18:04:19 +0300

    A Little Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, :email,  presence: true
-  has_many :addresses, :dependent => :destroy
-  accepts_nested_attributes_for :addresses, allow_destroy: true, reject_if: :all_blank
-
 end
